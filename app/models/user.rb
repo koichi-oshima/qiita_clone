18be96bef977eaaf09a5ad0ce9882abbe30f8dea
koichi-oshima <fairemygeneration@yahoo.co.jp>
2020-05-09 14:14:52 +0900

    add validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,36 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  allow_password_change  :boolean          default(FALSE)
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string(255)
+#  confirmed_at           :datetime
+#  email                  :string(255)
+#  encrypted_password     :string(255)      default(""), not null
+#  image                  :string(255)
+#  name                   :string(255)
+#  nickname               :string(255)
+#  provider               :string(255)      default("email"), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  tokens                 :text(65535)
+#  uid                    :string(255)      default(""), not null
+#  unconfirmed_email      :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_uid_and_provider      (uid,provider) UNIQUE
+#
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -10,4 +41,11 @@ class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, length: { in: 1..30 }
+
+  # 半角英数字大文字小文字をそれぞれ1文字以上含む
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,16}+\z/.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, length: { in: 5..20 }
 end
