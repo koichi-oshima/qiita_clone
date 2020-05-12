4ef5966d68116aa37410b0643fff996703ab0829
koichi-oshima <fairemygeneration@yahoo.co.jp>
2020-05-12 18:39:27 +0900

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -26,9 +26,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # namespaceを利用することで、 /api/v1/each_api 形式のrouting設定を行う
-  namespace :api, format: 'json' do
+  namespace :api, format: "json" do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for "User", at: "auth"
       resources :articles
     end
   end
