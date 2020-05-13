78bc6ce8307e34bf9426c3143bd48ba092546e9e
koichi-oshima <fairemygeneration@yahoo.co.jp>
2020-05-13 20:50:40 +0900

    fix dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 ActiveRecord::Base.transaction do
   20.times do |_n|
-    name = Faker::Name.name
+    name = Faker::Internet.username(specifier: 3..20) # バリデーションの文字数制限に当てはめる
     email = "#{_n}_" + Faker::Internet.email
-    password = Faker::Internet.password(min_length: 8)
+    password = Faker::Internet.password(min_length: 8, max_length: 20, mix_case: true)
     User.create!(
         name: name,
         email: email,
@@ -20,26 +20,26 @@ ActiveRecord::Base.transaction do
 
   100.times do
     title = Faker::Lorem.sentence
-    content = Faker::Lorem.paragraph
+    body = Faker::Lorem.paragraph
     user_id = User.pluck(:id).sample # sampleメソッドでUserテーブルのuser_idをランダム取得
     Article.create!(
         title: title,
-        content: content,
+        body: body,
         user_id: user_id,
         )
   end
 
   300.times do
-    content = Faker::Lorem.paragraph
-    user_ids = User.pluck(:id)  # pluckメソッドでUserテーブルのuser_idをarray取得
-    user_id = user_ids.sample　# sampleメソッドでuser_idsからuser_idをランダム取得
+    body = Faker::Lorem.paragraph
+    user_ids = User.pluck(:id) # pluckメソッドでUserテーブルのuser_idをarray取得
+    user_id = user_ids.sample # sampleメソッドでuser_idsからuser_idをランダム取得
     user_ids.delete(user_id)
     other_user_id = user_ids.sample
     article_id = Article.where(user_id: other_user_id).pluck(:id).sample
     next unless article_id # article_idがfalseの場合、次のループへ
 
     Comment.create!(
-        content: content,
+        body: body,
         user_id: user_id,
         article_id: article_id, #user_id以外のid(user_ids)が作成した記事に投稿する(投稿者と記事作成者が同一にならないようにする)
         )
