2e0ec8cb8c67dcaf9c56fb336663c2354c60aea8
koichi-oshima <fairemygeneration@yahoo.co.jp>
2020-05-14 22:47:10 +0900

    fix rspec

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -46,17 +46,18 @@ RSpec.describe "Api::V1::Articles", type: :request do
 
   describe "POST /api/v1/articles" do
     subject { post(api_v1_articles_path, params: params) }
+    let(:params) { {article: attributes_for(:article)} }
     let(:current_user) { create(:user) }
-    let(:params) { attributes_for(:user) }
-
+    # binding.pry
     before do
       allow_any_instance_of(Api::V1::Apicontroller).to receive(:current_user).and_return(current_user)
     end
+    # binding.pry
 
     it "記事を作成できる" do
       expect { subject }.to change { Article.count }.by(1)
       res = JSON.parse(response.body)
-      binding.pry
+      # binding.pry
       expect(res["title"]).to eq params[:article][:title]
       expect(res["body"]).to eq params[:article][:body]
       expect(response.status).to eq(200)
